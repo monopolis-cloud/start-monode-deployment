e71e271d78db619c39a864832bdf66351d784c5f
David Denton <denton.david@gmail.com>
2022-04-17 20:13:14 +0100

    do not attempt to start deployment when no deployment id

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ get_from_event() {
   jq -r "$1" "${GITHUB_EVENT_PATH}"
 }
 
-MONOPOLIS_URL="https://github-api.monopolis.cloud/deploy/start/$(get_from_event '.repository.full_name')/$(get_from_event '.inputs.deployment_id')"
+if echo "$1" | jq --exit-status '.inputs.deployment_id' >/dev/null; then
+  MONOPOLIS_URL="https://github-api.monopolis.cloud/deploy/start/$(get_from_event '.repository.full_name')/$(get_from_event '.inputs.deployment_id')"
+  echo ::set-output name=environment_map::$(curl --fail -X POST "${MONOPOLIS_URL}" -H "Authorization: Bearer ${GITHUB_TOKEN}")
+else
+  echo ::set-output name=environment_map::{}
+fi
 
-echo ::set-output name=environment_map::$(curl --fail \
-                                              -X POST "${MONOPOLIS_URL}" \
-                                              -H "Authorization: Bearer ${GITHUB_TOKEN}")
