f95de4e5c983efc468483b21d29fd3ff714c27b2
David Denton <denton.david@gmail.com>
2022-05-20 19:15:40 +0100

    change URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ get_from_event() {
 }
 
 if jq --exit-status '.inputs.deployment_id' "$GITHUB_EVENT_PATH" >/dev/null; then
-  MONOPOLIS_URL="https://github-api.monopolis.cloud/deploy/start/$(get_from_event '.repository.full_name')/$(get_from_event '.inputs.deployment_id')"
+  MONOPOLIS_URL="https://github-api.monopolis.cloud/rollout/start/$(get_from_event '.repository.full_name')/$(get_from_event '.inputs.deployment_id')"
   echo ::set-output name=environment_map::$(curl --fail -X POST "${MONOPOLIS_URL}" -H "Authorization: Bearer ${GITHUB_TOKEN}")
 else
   echo ::set-output name=environment_map::"{}"
