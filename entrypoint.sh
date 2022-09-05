828455eb133b1482dae90946219b51cc0529cad3
David Denton <denton.david@gmail.com>
2022-09-05 11:56:09 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,12 @@ if jq --exit-status '.inputs.deployment_id' "$GITHUB_EVENT_PATH" >/dev/null; the
   MONOPOLIS_URL="https://github-api.monopolis.cloud/rollout/start/$(get_from_event '.repository.full_name')/$(get_from_event '.inputs.deployment_id')"
 
   CONFIGURATIONS=$(curl --fail -X POST "${MONOPOLIS_URL}" -H "Authorization: Bearer ${GITHUB_TOKEN}")
-  echo ::set-output name=upstream-crosschecks::$UPSTREAM
-
-  UPSTREAM=$(echo "$CROSSCHECK_DATA" | jq 'to_entries[] | select(.value.type == "upstream") | .key')
   echo ::set-output name=configurations::$CONFIGURATIONS
 
-  DOWNSTREAM=$(echo "$CROSSCHECK_DATA" | jq 'to_entries[] | select(.value.type == "downstream") | .key')
+  UPSTREAM=$(echo "$CONFIGURATIONS" | jq 'to_entries[] | select(.value.type == "upstream") | .key')
+  echo ::set-output name=upstream-crosschecks::$UPSTREAM
+
+  DOWNSTREAM=$(echo "$CONFIGURATIONS" | jq 'to_entries[] | select(.value.type == "downstream") | .key')
   echo ::set-output name=downstream-crosschecks::$DOWNSTREAM
 else
   echo ::set-output name=configurations::"{}"
