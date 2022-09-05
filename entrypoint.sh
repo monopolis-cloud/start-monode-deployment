7fdd013137066056c6ca9714ad25114ff0613f4c
David Denton <denton.david@gmail.com>
2022-09-05 15:52:22 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,10 @@ if jq --exit-status '.inputs.deployment_id' "$GITHUB_EVENT_PATH" >/dev/null; the
   CONFIGURATIONS=$(curl --fail -X POST "${MONOPOLIS_URL}" -H "Authorization: Bearer ${GITHUB_TOKEN}")
   echo ::set-output name=configurations::$CONFIGURATIONS
 
-  UPSTREAM=$(echo "$CONFIGURATIONS" | jq 'if . == {} then [] else to_entries[] | select(.value.type == "upstream") | keys end')
+  UPSTREAM=$(echo "$CONFIGURATIONS" | jq 'if . == {} then [] else to_entries[] | select(.value.type == "upstream") | .key end')
   echo ::set-output name=upstream-crosschecks::$UPSTREAM
 
-  DOWNSTREAM=$(echo "$CONFIGURATIONS" | jq 'if . == {} then [] else to_entries[] | select(.value.type == "downstream") | keys end')
+  DOWNSTREAM=$(echo "$CONFIGURATIONS" | jq 'if . == {} then [] else to_entries[] | select(.value.type == "downstream") | .key end')
   echo ::set-output name=downstream-crosschecks::$DOWNSTREAM
 else
   echo ::set-output name=configurations::"{}"
