a90a3f3e1c836657e07ee5697857be48fe48633c
David Denton <denton.david@gmail.com>
2024-05-14 15:26:17 +0100

    Update entrypoint.sh to not use old style output in GHA

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,16 @@ if jq --exit-status '.inputs.ROLLOUT_DEPLOYMENT_ID' "$GITHUB_EVENT_PATH" >/dev/n
   MONOPOLIS_URL="https://api.monopolis.cloud/v1/rollout/start/$(get_from_event '.repository.name')/$(get_from_event '.inputs.ROLLOUT_DEPLOYMENT_ID')"
 
   CONFIGURATIONS=$(curl --fail -X POST "${MONOPOLIS_URL}" -H "Authorization: Bearer ${MONOPOLIS_API_KEY}")
-  echo ::set-output name=configurations::$CONFIGURATIONS
+  echo "configurations=${CONFIGURATIONS}" >> "$GITHUB_OUTPUT"
 
   UPSTREAM=$(echo "$CONFIGURATIONS" | jq 'to_entries | map(select(.value.type == "upstream")) | map(.key)')
-  echo ::set-output name=upstream-crosschecks::$UPSTREAM
+  echo "upstream-crosschecks=${UPSTREAM}" >> "$GITHUB_OUTPUT"
 
   DOWNSTREAM=$(echo "$CONFIGURATIONS" | jq 'to_entries | map(select(.value.type == "downstream")) | map(.key)')
-  echo ::set-output name=downstream-crosschecks::$DOWNSTREAM
+  echo "downstream-crosschecks=${DOWNSTREAM}" >> "$GITHUB_OUTPUT"
 
 else
-  echo ::set-output name=configurations::"{}"
-  echo ::set-output name=upstream-crosschecks::"[]"
-  echo ::set-output name=downstream-crosschecks::"[]"
+  echo 'configurations={}' >> "$GITHUB_OUTPUT"
+  echo 'upstream-crosschecks=[]' >> "$GITHUB_OUTPUT"
+  echo 'downstream-crosschecks=[]' >> "$GITHUB_OUTPUT"
 fi
